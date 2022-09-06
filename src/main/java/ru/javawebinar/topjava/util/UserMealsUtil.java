533beaf8044d61104b0107c2e3966ea9892f7018
Oleh Khyl <olegkhyll@gmail.com>
2022-09-06 16:00:24 +0300

    implement filteredByCycles method added isExcessCalories utility method

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,9 +3,11 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,12 +30,33 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with excess. Implement by cycles
-        return null;
+        List<UserMealWithExcess> filteredMeals = new ArrayList<>();
+
+
+        for (UserMeal meal : meals) {
+
+            if (TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime)) {
+                filteredMeals.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), isExcessCalories(meals, meal, caloriesPerDay)));
+            }
+        }
+        return filteredMeals;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO Implement by streams
         return null;
+    }
+
+    public static boolean isExcessCalories(List<UserMeal> mealList, UserMeal meal, int caloriesPerDay) {
+        int caloriesPerDaySum = 0;
+        LocalDate date = meal.getDateTime().toLocalDate();
+
+        for (UserMeal userMeal : mealList) {
+            if (userMeal.getDateTime().toLocalDate().equals(date)) {
+                caloriesPerDaySum += userMeal.getCalories();
+            }
+        }
+
+        return caloriesPerDaySum > caloriesPerDay;
     }
 }
