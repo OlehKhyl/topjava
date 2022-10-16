36ad88b25705ea0f2f41db7bfb0c9d83731e5172
Oleh Khyl <olegkhyll@gmail.com>
2022-10-16 13:22:14 +0300

    add user select

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -18,4 +18,11 @@ public class UserServlet extends HttpServlet {
         log.debug("forward to users");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Integer userId = Integer.parseInt(req.getParameter("user"));
+        SecurityUtil.setAuthUserId(userId);
+        resp.sendRedirect("meals");
+    }
 }
